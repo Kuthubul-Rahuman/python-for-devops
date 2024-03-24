9dc0c5360700e971f32ca327138c346f627b071f
Santosh Das <santosh.cloud003@gmail.com>
2024-03-24 18:30:19 +0530

    my test script

---
+++ Day-03/test.py
@@ -0,0 +1,25 @@
+a = 15
+b = 10
+
+def addition():
+    print(a + b)
+
+def sub():
+    print(a -b)
+
+addition()
+sub()
+
+
+####################
+
+def addition():
+    a = 15
+    b = 10
+    print(a + b)
+
+def sub():
+    print(a -b)
+
+addition()
+sub()
